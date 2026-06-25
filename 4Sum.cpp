02595e173add4b238e95fa4ed1565fc326a0c3b9
SURESHDOMA <69568120+sureshdoma123@users.noreply.github.com>
2026-06-25 21:29:14 +0530

    Implement fourSum function to find quadruplets

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -24,3 +24,38 @@ Constraints:
 
 Time Complexity: O(nlogn) + O(n2 * m)
 */
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>result;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if( i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if( j > i + 1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1, l = nums.size() - 1;
+                while( k < l)
+                {
+                    long long int sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if( sum == target)
+                    {
+                        vector<int>temp = {nums[i], nums[j], nums[k], nums[l]};
+                        result.emplace_back(temp);
+                        k++;
+                        l--;
+                        while( k < l && nums[k] == nums[k-1]) k++;
+                        while( k < l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if( sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+        return result;
+    }
+};
